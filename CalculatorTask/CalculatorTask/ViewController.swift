c9e6fb083602636980d0155dda3b3f0cf62d39d5
Joseph <yusuf.fayzullin@gmail.com>
2020-10-12 19:20:31 +0300

    Initial Commit

---
+++ CalculatorTask/CalculatorTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorTask
+//
+//  Created by Joseph on 12.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
